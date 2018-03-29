9d891a547d038253fa31c0247d2be3ccafe134ce
Taparna <thirumalagiriaparna@gmail.com>
2018-03-29 12:28:25 +0530

    modified files

--- main.js
+++ main.js
@@ -14,7 +14,9 @@ xhr.send();
 loadjson("data.json",function (text) {
   let data=JSON.parse(text);
   console.log(data);
-  basics(data.left);
+  basics(data.basics);
+  education(data.education);
+  skill(data.skills);
 })
 //for main div calling
 //var main=document.queryselector('.main');
@@ -47,4 +49,47 @@ var ph=document.createElement("p");
 ph.textContent=leftside.phone;
 email.appendChild(ph);
 console.log(left);
+}
+
+var right=document.createElement("div");
+right.classList.add("right");
+main.appendChild(right);
+//education div start
+var edu=document.createElement("div");
+edu.classList.add("edu1");
+edu.textContent="Eucation Details:";
+edu.appendChild(document.createElement("HR"));
+right.appendChild(edu);
+function education(Educa) {
+  for (i in Educa) {
+    var e1=document.createElement("div");
+    e1.classList.add("edu2");
+    e1.textContent=Educa[i].course;
+
+    var ul=document.createElement("ul");
+    for(j in Educa[i].college){
+    var li=document.createElement("li");
+    li.textContent=Educa[i].college[j];//each object lo college
+    ul.appendChild(li);
+    e1.appendChild(ul);
+    edu.appendChild(e1);
+    }
+  }
+}
+function skill(skills){
+  var skill_title=document.createElement("div");
+  skill_title.classList.add("tech_skills");
+  skill_title.textContent="Techanical skills";
+  skill_title.appendChild(document.createElement("HR"));
+  right.appendChild(skill_title);
+  //table creation
+  var table=document.createElement("table");
+  // table.setAttribute("id","table");
+   var row="";
+   for (var i = 0; i < skills.length; i++) {
+     row =row+"<tr><td>"+skills[i].name+"</td><td>"+skills[i].value+"</td></tr>"
+   }
+   table.innerHTML=row;
+   skill_title.appendChild(table);
+
 }
